a258a1131adc7ee5f2dc13c3dd2be78547cda946
Jose Bocanegra <j.bocanegra@uniandes.edu.co>
2022-08-02 13:02:33 -0500

    Add controllers

---
+++ src/museum-artwork/museum-artwork.controller.ts
@@ -0,0 +1,37 @@
+/* eslint-disable prettier/prettier */
+import { Body, Controller, Delete, Get, HttpCode, Param, Post, Put, UseInterceptors } from '@nestjs/common';
+import { ArtworkDto } from '../artwork/artwork.dto';
+import { BusinessErrorsInterceptor } from '../shared/interceptors/business-errors.interceptor';
+import { MuseumArtworkService } from './museum-artwork.service';
+
+@Controller('museums')
+@UseInterceptors(BusinessErrorsInterceptor)
+export class MuseumArtworkController {
+    constructor(private readonly museumArtworkService: MuseumArtworkService){}
+
+    @Post(':museumId/artworks/:artworkId')
+    @HttpCode(201)
+    async addArtworkMuseum(@Param('museumId') museumId: string, @Param('artworkId') artworkId: string){
+        return await this.museumArtworkService.addArtworkMuseum(museumId, artworkId);
+    }
+
+    @Get(':museumId/artworks/:artworkId')
+    async findArtworkByMuseumIdArtworkId(@Param('museumId') museumId: string, @Param('artworkId') artworkId: string){
+        return await this.findArtworkByMuseumIdArtworkId(museumId, artworkId);
+    }
+
+    @Get(':museumId/artworks')
+    async findArtworksByMuseumId(@Param('museumId') museumId: string){
+        return await this.findArtworksByMuseumId(museumId);
+    }
+
+    @Put(':museumId/artworks')
+    async associateArtworksMuseum(@Body() artworksDto: ArtworkDto[], @Param('museumId') museumId: string){
+        return await this.associateArtworksMuseum(artworksDto, museumId);
+    }
+    
+    @Delete(':museumId/artworks/:artworkId')
+    async deleteArtworkMuseum(@Param('museumId') museumId: string, @Param('artworkId') artworkId: string){
+        return await this.deleteArtworkMuseum(museumId, artworkId);
+    }
+}
